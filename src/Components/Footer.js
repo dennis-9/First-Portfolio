e744e7b6984506307dcc490a5a3a9753d85ee558
Dennis Asiedu <asiedudennis30@gmail.com>
2024-07-24 14:09:19 +0000

    Updated My Part

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,41 +1,52 @@
 import React from "react";
 
 const Footer = ({ data }) => {
-  if (data) {
-    var networks = data.social.map(function (network) {
-      return (
-        <li key={network.name}>
-          <a href={network.url}>
-            <i className={network.className}></i>
-          </a>
-        </li>
-      );
-    });
-  }
+    if (data) {
+        var networks = data.social.map(function(network) {
+            return ( <
+                li key = { network.name } >
+                <
+                a href = { network.url } >
+                <
+                i className = { network.className } > < /i> < /
+                a > <
+                /li>
+            );
+        });
+    }
 
-  return (
-    <footer>
-      <div className="row">
-        <div className="twelve columns">
-          <ul className="social-links">{networks}</ul>
+    return ( <
+        footer >
+        <
+        div className = "row" >
+        <
+        div className = "twelve columns" >
+        <
+        ul className = "social-links" > { networks } < /ul>
 
-          <ul className="copyright">
-            <li>
-              Made by{" "}
-              <a title="PAPA" href="http://www.papareact.com/">
-                PAPA
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="go-top">
-          <a className="smoothscroll" title="Back to Top" href="#home">
-            <i className="icon-up-open"></i>
-          </a>
-        </div>
-      </div>
-    </footer>
-  );
+        <
+        ul className = "copyright" >
+        <
+        li >
+        Made by { " " } <
+        a title = "DENNIS"
+        href = "https://www.instagram.com/dennis_techie?utm_source=qr&igsh=NG1qZm9seDN2Yjkx" >
+        DENNIS < /a> < /
+        li > <
+        /ul> < /
+        div > <
+        div id = "go-top" >
+        <
+        a className = "smoothscroll"
+        title = "Back to Top"
+        href = "#home" >
+        <
+        i className = "icon-up-open" > < /i> < /
+        a > <
+        /div> < /
+        div > <
+        /footer>
+    );
 };
 
 export default Footer;
